fd4476654547138fd64dee25522c2530dc41f61e
ScrapWare <145694110+ScrapWare@users.noreply.github.com>
2023-10-17 10:27:45 +0900

    Add files via upload

---
+++ sd-get-prompt.c
@@ -0,0 +1,262 @@
+#include <sd-get-prompt.h>
+
+SDtEXt get_tEXt(char *path){
+
+  SDtEXt  gTXt = {
+    0, 0, 0, 0, 0, 0, 0, 0, "\0\0\0\0\0", "\0", 0, NULL, 0, NULL
+  };
+
+  char  buf[9] = "\0\0\0\0\0\0\0\0\0";
+
+  FILE *fp;
+
+  int i;
+
+  /* Open */
+  fp = fopen(path, "rb");
+
+  if(fp == NULL){
+    gTXt.eMEs = ERR_NO_OPEN;
+    return gTXt;
+  }
+
+  /* PNG Header */
+  if(! fread(buf, 1, 8, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  if(strcmp(PMG_FHDR, buf)){
+    gTXt.eMEs = ERR_NO_PNGH;
+    return gTXt;
+  }
+
+  /* IHDR Chunk */
+  if(! fread(buf, 1, 8, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  if(strcmp(PNG_IHDR, buf)){
+    gTXt.eMEs = ERR_NO_IHDR;
+    return gTXt;
+  }
+
+  /* Width IHDR Chunk */
+  if(! fread(&i, 4, 1, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  gTXt.width = bswap_32(i);
+
+  /* Height IHDR Chunk */
+  if(! fread(&i, 4, 1, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  gTXt.height = bswap_32(i);
+
+  /* Depth IHDR Chunk */
+  if(! fread(&gTXt.depth, 1, 1, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  /* Color IHDR Chunk */
+  if(! fread(&gTXt.color, 1, 1, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  /* Compression IHDR Chunk */
+  if(! fread(&gTXt.compression, 1, 1, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  /* Filter IHDR Chunk */
+  if(! fread(&gTXt.filter, 1, 1, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  /* Interlace IHDR Chunk */
+  if(! fread(&gTXt.interlace, 1, 1, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  /* CRC IHDR Chunk */
+  if(! fread(&gTXt.ihdr_crc, 1, 4, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  /* tEXt Chunk 
+  \0\0\0\0tEXtparameters(Prompt)\n
+  Negative prompt:(Negative Prompt)\n
+  Steps: ~~~~~~~~ Version: version(\0)
+  */
+
+  /* Size of tEXt Chunk */
+  if(! fread(&i, 4, 1, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  gTXt.size = bswap_32(i);
+
+  /* Identifier tEXt Chunk */
+  if(! fread(&gTXt.tEXt, 1, 4, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  /* Check tEXt identifier */
+
+  if(strcmp(SD_tEXt_Stable, gTXt.tEXt)){
+    gTXt.made = SD_Made_Stable;
+  } else if(strcmp(SD_tEXt_Meitu1, gTXt.tEXt)){
+    gTXt.made = SD_Made_Meitu1;
+  } else if(strcmp(SD_tEXt_Meitu2, gTXt.tEXt)){
+    gTXt.made = SD_Made_Meitu2;
+  } else{
+    gTXt.eMEs = ERR_NO_tEXt;
+    return gTXt;
+  }
+
+  /* Malloc */
+  gTXt.param = (char*)malloc(sizeof(char)*gTXt.size+1);
+
+  if(gTXt.param == NULL){
+    gTXt.eMEs = ERR_NO_ALOC;
+    return gTXt;
+  }
+
+  /* Parameters of tEXt Chunk */
+  if(! fread(gTXt.param, 1, gTXt.size, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  /* Check Parameters of tEXt Chunk */
+  if(! strncmp(SD_tEXt_Param, gTXt.param, 11)){
+    printf("tEXt parameter check fail\n");
+    gTXt.eMEs = ERR_NO_PARA;
+    return gTXt;
+  }
+
+  /* CRC IHDR Chunk */
+  if(! fread(&gTXt.tEXt_crc, 1, 4, fp)){
+    gTXt.eMEs = ERR_NO_READ;
+    return gTXt;
+  }
+
+  /* Close */
+  fclose(fp);
+
+  return gTXt;
+
+}
+
+int iTXt_dialog(int GTK_MESSAGE_TYPE, const char *msg){
+
+  int result;
+
+  if(GTK_MESSAGE_TYPE != GTK_MESSAGE_INFO){
+    fprintf(stderr, "%s: %s\n", APPNAME, msg);
+  } else{
+    fprintf(stdout, "%s: %s\n", APPNAME, msg);
+  }
+
+  GtkWidget *iDlg = gtk_message_dialog_new(
+    NULL,
+    GTK_DIALOG_MODAL,
+    GTK_MESSAGE_TYPE,
+    GTK_BUTTONS_OK,
+    APPLABL
+  );
+
+  gtk_window_set_title(GTK_WINDOW(iDlg), APPNAME);
+  gtk_message_dialog_format_secondary_text(GTK_MESSAGE_DIALOG(iDlg), msg);
+  result = gtk_dialog_run(GTK_DIALOG(iDlg));
+  gtk_widget_destroy(iDlg);
+
+  return result;
+
+}
+
+int main(int argc, char *argv[]){
+
+  SDtEXt iTXt;
+
+  char *neg;
+  char *inf;
+  char *tmp;
+  char *dsp;
+
+  int lens[3];
+
+  int ret;
+
+  GtkWidget *iWin;
+
+  gtk_init(&argc, &argv);
+
+  if(argc <2){
+    iTXt_dialog(GTK_MESSAGE_ERROR, ERR_NO_ARGC);
+    return 1;
+  }
+
+  iTXt = get_tEXt(argv[1]);
+
+  if(iTXt.eMEs != NULL){
+    if(iTXt.param != NULL){ free(iTXt.param); }
+    iTXt_dialog(GTK_MESSAGE_WARNING, iTXt.eMEs);
+    return 1;
+  }
+
+  tmp = iTXt.param + 11;
+
+  /* Search Info */
+  if((inf = strstr(tmp, STR_INF)) == NULL){
+    if(iTXt.param != NULL){ free(iTXt.param); }
+    iTXt_dialog(GTK_MESSAGE_WARNING, ERR_NG_INFO);
+    return 1;
+  }
+
+  *inf = 0; inf++;
+  lens[2] = strlen(inf);
+
+  /* Search Negative */
+  if((neg = strstr(tmp, STR_NEG)) == NULL){
+    if(iTXt.param != NULL){ free(iTXt.param); }
+    iTXt_dialog(GTK_MESSAGE_WARNING, ERR_NG_NGTV);
+    return 1;
+  }
+
+  *neg = 0; neg++;
+  lens[1] = strlen(neg);
+
+  lens[0] = strlen(tmp);
+
+  /* Malloc */
+  dsp = (char*)malloc(sizeof(char)*(lens[0]+lens[2]+3));
+  /* Copy */
+  strcpy(dsp, tmp);
+  strcat(dsp, "\n\n");
+  strcat(dsp, inf);
+
+  free(iTXt.param);
+
+  iTXt_dialog(GTK_MESSAGE_INFO, dsp);
+
+  free(dsp);
+
+  return 0;
+
+}
+
+/* __END__ */
---
+++ sd-get-prompt.h
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <byteswap.h>
+
+#include <gtk/gtk.h>
+
+#define APPNAME "sd-get-prompt"
+#define APPLABL "SD Created Info"
+
+#define STR_NEG "\nNegative prompt:\x20"
+#define STR_INF "\nSteps:\x20"
+
+#define PMG_FHDR "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a\0"
+#define PNG_IHDR "\x00\x00\x00\x0d\x49\x48\x44\x52\0"
+
+#define ERR_NO_ARGC "Few to arguments!\0"
+#define ERR_NO_OPEN "Failed open file!\0"
+#define ERR_NO_READ "Failed read file!\0"
+#define ERR_NO_ALOC "Failed allocation memory!\0"
+#define ERR_NO_PNGH "Invalid PNG signature!\0"
+#define ERR_NO_IHDR "Invalid IHDR chunk!\0"
+#define ERR_NO_tEXt "Not a tEXt chunk!\0"
+#define ERR_NO_PARA "Not a Stable Diffusion Parameters!\0"
+
+#define ERR_NG_INFO "Invalid information text!\0"
+#define ERR_NG_NGTV "Invalid negative prompt!\0"
+
+#define SD_tEXt_Param "Parameters\0"
+
+#define SD_tEXt_Stable "tEXt\0"
+#define SD_tEXt_Meitu1 "iTXt\0"
+#define SD_tEXt_Meitu2 "rTXt\0"
+
+#define SD_Made_Stable "SD tEXt\0"
+#define SD_Made_Meitu1 "Meitu iTXt\0"
+#define SD_Made_Meitu2 "Meitu rTXt\0"
+
+typedef struct _ihdr{
+  int  width;
+  int  height;
+  char depth;
+  char color;
+  char compression;
+  char filter;
+  char interlace;
+  int  ihdr_crc;
+}; ihdr, *pihdr;
+
+typedef struct _tEXtP{
+  char  tEXt[5];
+  char *made;
+  int   size;
+  char *param;
+  int   tEXt_crc;
+  char *eMEs;
+}; tEXtP, *ptEXtP;
+
+typedef struct _SDtEXt{
+  /* IHDR */
+  int   width;
+  int   height;
+  char  depth;
+  char  color;
+  char  compression;
+  char  filter;
+  char  interlace;
+  int   ihdr_crc;
+  /* tEXt */
+  char  tEXt[5];
+  char *made;
+  int   size;
+  char *param;
+  int   tEXt_crc;
+  char *eMEs;
+} SDtEXt, *pSDtEXt;
+
+/* End of Header */
